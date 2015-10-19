5c9f335475800f20b63e34226c92f79716ee88df
AleksJev <aleksejs.jevsejevs@gmail.com>
2015-10-20 00:47:21 +0300

    RAM оперативная память, вид изнутри

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,13 +10,62 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            Func1(x);
+            Console.WriteLine("Call Func1. x = " + x ); // Call Func1. x = 1
+
+            x = 1;
+            Func2(x);
+            Console.WriteLine("Call Func2. x = " + x); // Call Func2. x = 1
+
+            x = 1;
+            Func3(x);
+            Console.WriteLine("Call Func3. x = " + x); // Call Func3. x = 1
+
+
             Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Move(p1, 10, 10);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y); // Call Move. p1.x = 11, p1.y = 13
+            
 
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
-           
+            p1 = p2;
+            p2.x = 8;
+            p2.y = 8;
+            Console.WriteLine("p1 = p2. p1.x = " + p1.x + ", p1.y = " + p1.y + "; p2.x = " + p2.x + ", p2.y = " + p2.y); //p1 = p2. p1.x = 8, p1.y = 8; p2.x = 8, p2.y = 8
+
+            p1 = new Point(1, 3, '*');
+            Update(p1);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y); // Call Move. p1.x = 1, p1.y = 1
+
+
             Console.ReadLine();
+        }
+
+        public static void Func1(int value)
+        {
+
+        }
+
+        public static void Func2(int value)
+        {
+            value = value + 1;
+        }
+
+        public static void Func3(int X)
+        {
+            X = X + 1;
+        }
+
+        public static void Move(Point p, int dx, int dy)
+        {
+            p.x = p.x + dx;
+            p.y = p.y + dy;
+        }
+
+        public static void Update(Point p)
+        {
+            p = new Point();
         }
         
     }
