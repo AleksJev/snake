e3666c14ec031ec742e4313837a0b2288eb6ea62
AleksJev <aleksejs.jevsejevs@gmail.com>
2015-10-21 22:16:03 +0300

    lesson 6

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,63 +10,43 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x = 1;
-            Func1(x);
-            Console.WriteLine("Call Func1. x = " + x ); // Call Func1. x = 1
-
-            x = 1;
-            Func2(x);
-            Console.WriteLine("Call Func2. x = " + x); // Call Func2. x = 1
-
-            x = 1;
-            Func3(x);
-            Console.WriteLine("Call Func3. x = " + x); // Call Func3. x = 1
-
-
-            Point p1 = new Point(1, 3, '*');
-            Move(p1, 10, 10);
-            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y); // Call Move. p1.x = 11, p1.y = 13
             
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+                   
 
             Point p2 = new Point(4, 5, '#');
-            p1 = p2;
-            p2.x = 8;
-            p2.y = 8;
-            Console.WriteLine("p1 = p2. p1.x = " + p1.x + ", p1.y = " + p1.y + "; p2.x = " + p2.x + ", p2.y = " + p2.y); //p1 = p2. p1.x = 8, p1.y = 8; p2.x = 8, p2.y = 8
+            p2.Draw();
 
-            p1 = new Point(1, 3, '*');
-            Update(p1);
-            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y); // Call Move. p1.x = 1, p1.y = 1
+            Point p3 = new Point(7, 7, '$');
+            Point p4 = new Point(12, 15, '@');
+            Point p5 = new Point(17, 25, '&');
+            List<int> numLIst = new List<int>();
+            numLIst.Add(0);
+            numLIst.Add(1);
+            numLIst.Add(2);
 
+            int x = numLIst[0];
+            int y = numLIst[1];
+            int z = numLIst[2];
+
+            foreach (int i in numLIst)
+            {
+                Console.WriteLine(i);
+            }
+
+            numLIst.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+            pList.Add(p5);
 
             Console.ReadLine();
         }
-
-        public static void Func1(int value)
-        {
-
-        }
-
-        public static void Func2(int value)
-        {
-            value = value + 1;
-        }
-
-        public static void Func3(int X)
-        {
-            X = X + 1;
-        }
-
-        public static void Move(Point p, int dx, int dy)
-        {
-            p.x = p.x + dx;
-            p.y = p.y + dy;
-        }
-
-        public static void Update(Point p)
-        {
-            p = new Point();
-        }
+        
         
     }
 }
